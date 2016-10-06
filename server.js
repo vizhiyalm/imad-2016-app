7320088600c2e3b0cf1df38b63d45dcc79b364ad
kayalvizhi <vizhiyalm@gmail.com>
2016-10-06 18:43:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/artiOne', function (req, res) {
+  res.send("This is first");
+});
+
+app.get('/artiTwo', function (req, res) {
+  res.send("This is second");
+});
+
+app.get('/artiThree', function (req, res) {
+  res.send("This is third");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
