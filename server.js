975de60eedb53e05235736ccf9012a93410e44d6
kayalvizhi <vizhiyalm@gmail.com>
2016-10-08 23:14:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var counter=0;
 app.get('/counter', function (req, res) {
     counter=counter+1;
     res.send(counter.toString());
