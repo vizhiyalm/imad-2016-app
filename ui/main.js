53c190b9af650f91b2da39999edd4899f089012d
kayalvizhi <vizhiyalm@gmail.com>
2016-10-08 19:09:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,19 @@
-console.log('Loaded!');
+var button =document.getElementById('counter');
+button.onclick= function(){
+    //create a request object
+    var request=new XMLHttpRequest();
+    
+    //capture the response and store it in a var
+    request.onreadystatechange =function(){
+        if (request.readyState === XMLHttpRequest.DONE ){
+            if(request.status=== 200){
+                var counter=request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML=counter.toString():
+            }
+        }
+    }
+    // make the request
+    request.open('GET','http://vizhiyalm.imad.hasura-app.io/counter',true);
+    request.send(null);
+}
