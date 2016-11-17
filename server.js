c27cb34b837985864a05a19fe4105f9a1784cee9
kayalvizhi <vizhiyalm@gmail.com>
2016-11-17 22:32:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get('/ui/carme.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'carme.jpg'));
 });
 
-et('/ui/style.css', function (req, res) {
+app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
