ab84d14bb7bf730f6aaa6ada8849934886ee3724
kayalvizhi <vizhiyalm@gmail.com>
2016-11-18 20:14:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,27 +13,11 @@
    res.sendFile(path.join(__dirname, 'ui', 'blog.html'));
  });
 
-app.get('/images/wisdomSchool.jpg', function (req, res) {
-    res.sendFile(path.join(__dirname, 'images', 'wisdomSchool.jpg'));
-});
-
-
-app.get('/images/anchorHospital.jpg', function (req, res) {
-    res.sendFile(path.join(__dirname, 'images', 'anchorHospital.jpg'));
-});
-
-app.get('/images/dashboard.jpg', function (req, res) {
-    res.sendFile(path.join(__dirname, 'images', 'dashboard.jpg'));
-});
 
 app.get('ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('ui/slideshow1.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'slideshow1.js'));
-});
 
-app.get('/ui/behance.png', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'behance.png'));
- });	
+
+	
