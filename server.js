8197a242ccff894acf2c295e62537a2426619f62
kayalvizhi <vizhiyalm@gmail.com>
2016-11-18 12:04:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 
 var config = {
-    user: 'coco98',
-    database: 'coco98',
+    user: 'vizhiyalm',
+    database: 'vizhiyalm',
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
