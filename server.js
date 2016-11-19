9ebde2a8b6a56306b47f6003c22cfd8240669e1f
kayalvizhi <vizhiyalm@gmail.com>
2016-11-19 20:22:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/article-db', function (req, res) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
-          res.send(JSON.stringify());
+          res.send(JSON.stringify(result));
       }
    });
 });
