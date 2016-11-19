1d7f9887ab07f7910984bcd94ba8507ba34b7788
kayalvizhi <vizhiyalm@gmail.com>
2016-11-19 20:17:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,21 @@
  app.get('/', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
  });
+ 
+ var pool = new Pool(config);
+
+app.get('/article-db', function (req, res) {
+   // make a select request
+   // return a response with the results
+   pool.query('SELECT * FROM article', function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify());
+      }
+   });
+});
+
 
  app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
