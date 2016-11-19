0f90cfea6aad53a8d20390bef3422ef1870d5570
kayalvizhi <vizhiyalm@gmail.com>
2016-11-19 07:30:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@
  app.get('/', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
  });
+ 
+  app.get('ui/main.js', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+ });
 
 app.get('/ui/wisdomSchool.jpg', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'wisdomSchool.jpg'));
