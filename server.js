e176fdc5def47952317278029e327ba620fcbacf
kayalvizhi <vizhiyalm@gmail.com>
2016-11-22 20:55:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,40 @@ function createTemplate (data) {
     var content = data.content;
     
     var htmlTemplate = `
-   
+    // <html>
+    //   <head>
+    //       <title>
+    //           ${title}
+    //       </title>
+    //       <meta name="viewport" content="width=device-width, initial-scale=1" />
+    //       <link href="/ui/style.css" rel="stylesheet" />
+    //   </head> 
+    //   <body>
+    //       <div class="container">
+    //           <div>
+    //               <a href="/">Home</a>
+    //           </div>
+              
+    //           <h3>
+    //               ${heading}
+    //           </h3>
+    //           <div>
+    //               ${date.toDateString()}
+    //           </div>
+    //           <div>
+    //             ${content}
+    //           </div>
+             
+    //           <!--<h4>Comments</h4>
+    //           <div id="comment_form">
+    //           </div>
+    //           <div id="comments">
+    //             <center>Loading comments...</center>
+    //           </div>--->
+    //       </div>
+    //       <script type="text/javascript" src="/ui/article.js"></script>
+    //   </body>
+    // </html>
     
     
     <!DOCTYPE html>
