2887ba33dd569f22b4fe09b1dcf8743b74eec3f2
kayalvizhi <vizhiyalm@gmail.com>
2016-11-27 05:46:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,6 +92,35 @@ function createTemplate (data) {
    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
  });
  
+ function hash (input, salt) {
+    // How do we create a hash?
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
+}
+
+
+app.get('/hash/:input', function(req, res) {
+   var hashedString = hash(req.params.input, 'this-is-some-random-string');
+   res.send(hashedString);
+});
+
+app.post('/create-user', function (req, res) {
+   // username, password
+   // {"username": "tanmai", "password": "password"}
+   // JSON
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.randomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send('User successfully created: ' + username);
+      }
+   });
+});
+ 
  var pool = new Pool(config);
 
 app.get('/article-db', function (req, res) {
