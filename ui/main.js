ebbcb42e5725386cf808fd67ae512f9a4d23844b
kayalvizhi <vizhiyalm@gmail.com>
2016-11-30 09:13:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -127,10 +127,10 @@ function loadArticles () {
                     
              
                 }
-                content += "</ul>"
+                content += "</ul>";
                 articles.innerHTML = content;
             } else {
-                articles.innerHTML('Oops! Could not load all articles!')
+                articles.innerHTML('Oops! Could not load all articles!');
             }
         }
     };
