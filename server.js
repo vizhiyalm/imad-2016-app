89e611d04ad3a523146d9db32c27c6b125db5a0f
kayalvizhi <vizhiyalm@gmail.com>
2016-12-05 10:18:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -288,10 +288,6 @@ app.get('/articles/:articleName', function (req, res) {
            
 
 
-//  app.get('/ui/style.css', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-//  }); 
- 
  app.get('/ui/:fileName', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
 });
